e1cc2c8239de45b4fc3ceafcd885e70ba681ff7c
vtemian <vladtemian@gmail.com>
2014-01-05 17:44:15 +0200

    Implement proteins generator

--- c/0-folding/folding.c
+++ c/0-folding/folding.c
@@ -5,6 +5,37 @@ struct point{
   int x, y;
 } stack[100];
 
+char string[15];
+
+int fill(int n, struct point stack[], int str_len) {
+  int i, j;
+  char proteins[] = "HP";
+
+  if (str_len == n){
+    printf("%s\n", string);
+    return -1;
+  }
+
+  for(i=0; i<=1; i++){
+    string[str_len] = proteins[i];
+    string[str_len+1] = '\0';
+
+    fill(n, stack, str_len+1);
+  }
+}
+
+void init_fill(int n) {
+  int i;
+  struct point stack_copy[100];
+
+  // copy stack
+  for(i=0; i<n; i++) {
+    stack_copy[i] = stack[i];
+  }
+
+  fill(n, stack_copy, 0);
+}
+
 int check_point(int x, int y, int n, int stack_size) {
   if (x > 2*n-1 || y > 2*n-1 || x < 0 || y < 0) return 0;
   if (stack_size >= n) return 0;
@@ -25,10 +56,7 @@ int generate_fold(int n, int x, int y, int stack_size) {
   if (stack_size == n){
     stack_size--;
 
-    printf("\n");
-    for(i=0; i<n; i++){
-      printf("%d %d\n", stack[i].x, stack[i].y);
-    }
+    init_fill(n);
 
     stack[n].x = 0;
     stack[n].y = 0;
@@ -43,6 +71,7 @@ int generate_fold(int n, int x, int y, int stack_size) {
 }
 
 int main() {
+
   int n = 3;
   generate_fold(n, n-1, n-1, 0);
 
