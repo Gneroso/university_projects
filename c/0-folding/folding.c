4eb1eadbd77be5dfe31d1317240b8e96f7eca29a
vtemian <vladtemian@gmail.com>
2014-01-05 23:14:29 +0200

    Calculate all the connecting points

--- c/0-folding/folding.c
+++ c/0-folding/folding.c
@@ -3,16 +3,47 @@
 
 struct point{
   int x, y;
-} stack[100];
+} stack[10000];
 
-char string[15];
+char string[150];
+int optim;
+
+int calculate_optim(int n, struct point stack[], char string[]) {
+  int i, j, optimal=0;
+  int fold[2*n-1][2*n-1];
+  struct point p;
+
+  for(i=0; i<n; i++) {
+    for(j=0; j<n; j++) {
+      fold[i][j] = 0;
+    }
+  }
+
+  for(i=0; i<n; i++) {
+    if (string[i] == 'H')
+      fold[stack[i].x][stack[i].y] = 1;
+  }
+
+  for(i=0; i<n; i++) {
+    if (string[i] == 'H'){
+      p = stack[i];
+      if (fold[p.x+1][p.y]) optimal++;
+      if (fold[p.x][p.y+1]) optimal++;
+      if (fold[p.x-1][p.y]) optimal++;
+      if (fold[p.x][p.y-1]) optimal++;
+    }
+  }
+
+  return optimal/2;
+
+}
 
 int fill(int n, struct point stack[], int str_len) {
   int i, j;
   char proteins[] = "HP";
 
   if (str_len == n){
-    printf("%s\n", string);
+    optim += calculate_optim(n, stack, string);
     return -1;
   }
 
@@ -72,8 +103,9 @@ int generate_fold(int n, int x, int y, int stack_size) {
 
 int main() {
 
-  int n = 3;
+  int n = 8;
   generate_fold(n, n-1, n-1, 0);
+  printf("%d", optim/256);
 
   return 0;
 }
