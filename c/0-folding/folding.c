44070d07411f7ccb23ad1881bfc6e24c0ef9d821
vtemian <vladtemian@gmail.com>
2014-01-08 10:39:03 +0200

    Generate a correct answer

--- c/0-folding/folding.c
+++ c/0-folding/folding.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 #include <math.h>
+#include <string.h>
 
 struct point{
   int x, y;
 } stack[10000];
 
-char string[150];
-int optim;
+char string[15];
+char proteins[32768][16];
+
+int optim[32768], combination;
 
 int calculate_optim(int n, struct point stack[], char string[]) {
   int i, j, optimal=0;
   int fold[2*n-1][2*n-1];
   struct point p;
 
-  for(i=0; i<n; i++) {
-    for(j=0; j<n; j++) {
+  for(i=0; i<2*n-1; i++) {
+    for(j=0; j<2*n-1; j++) {
       fold[i][j] = 0;
     }
   }
@@ -35,28 +40,27 @@ int calculate_optim(int n, struct point stack[], char string[]) {
   }
 
   return optimal/2;
-
 }
 
-int fill(int n, struct point stack[], int str_len) {
-  int i, j;
-  char proteins[] = "HP";
+int generate_proteins(int n, int str_len) {
+  int i;
+  char raw_proteins[] = "HP";
 
   if (str_len == n){
-    optim += calculate_optim(n, stack, string);
+    strcpy(proteins[combination++], string);
     return -1;
   }
 
   for(i=0; i<=1; i++){
-    string[str_len] = proteins[i];
+    string[str_len] = raw_proteins[i];
     string[str_len+1] = '\0';
 
-    fill(n, stack, str_len+1);
+    generate_proteins(n, str_len+1);
   }
 }
 
 void init_fill(int n) {
-  int i;
+  int i, result;
   struct point stack_copy[100];
 
   // copy stack
@@ -64,12 +68,22 @@ void init_fill(int n) {
     stack_copy[i] = stack[i];
   }
 
-  fill(n, stack_copy, 0);
+  for(i=0; i<pow(2, n); i++) {
+    result = calculate_optim(n ,stack, proteins[i]);
+
+    if (result > optim[i]) optim[i] = result;
+  }
 }
 
 int check_point(int x, int y, int n, int stack_size) {
+  int i;
+
   if (x > 2*n-1 || y > 2*n-1 || x < 0 || y < 0) return 0;
   if (stack_size >= n) return 0;
+
+  // check only last point
+  for (i=0; i<stack_size; i++)
+    if (stack[i].x == x && stack[i].y == y) return 0;
 
   return 1;
 }
@@ -81,8 +95,9 @@ int generate_fold(int n, int x, int y, int stack_size) {
     stack[stack_size].x = x;
     stack[stack_size].y = y;
     stack_size++;
-  }else
+  }else{
     return -1;
+  }
 
   if (stack_size == n){
     stack_size--;
@@ -102,10 +117,17 @@ int generate_fold(int n, int x, int y, int stack_size) {
 }
 
 int main() {
+  int n=8, s=0, i;
 
-  int n = 8;
+  generate_proteins(n, 0);
+
   generate_fold(n, n-1, n-1, 0);
-  printf("%d", optim/256);
+
+  for(i=0; i<258; i++) {
+    s+=optim[i];
+  }
+
+  printf("\n%d", s);
 
   return 0;
 }
