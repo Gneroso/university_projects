58cbb8b3dbe8d1275743c3fd5d043cfa12914cb3
vtemian <vladtemian@gmail.com>
2014-04-19 20:34:03 +0300

    Use a separate method to get all peers of a torrent

--- data_structures/bitorrent/client.py
+++ data_structures/bitorrent/client.py
@@ -1,3 +1,5 @@
+import urllib
+from random import randint
 from urlparse import urlparse
 
 from torrent import Torrent
@@ -7,6 +9,9 @@ from trackers.udp import UDPTracker
 class Client(object):
   __TORRENTS = {}
 
+  def __init__(self):
+    self.peer_id = urllib.quote("-AZ2470-" + "".join([str(randint(0, 9)) for i in xrange(12)]))
+
   @property
   def torrents(self):
     return self.__TORRENTS
@@ -15,14 +20,23 @@ class Client(object):
   def torrents(self, new_torrent):
     self.__TORRENTS[new_torrent] = Torrent(new_torrent)
 
+  def _get_peers(self, torrent):
+    peers = {}
+
+    for url in torrent.urls:
+      parsed = urlparse(url)
+      if parsed.scheme == 'udp':
+        _, url, port = url.split(":")
+        tracker = UDPTracker(url[2:], int(port), torrent, self.peer_id)
+
+        peers.update({ip: port for ip, port in tracker.peers})
+
+    return peers
+
   def download(self, torrent):
     if not torrent in self.__TORRENTS:
       raise ValueError('%s not here' % torrent)
 
     torrent = self.__TORRENTS[torrent]
-    for url in torrent.urls:
-      parsed = urlparse(url)
-      if parsed.scheme == 'udp':
-        _, url, port = url.split(":")
-        tracker = UDPTracker(url[2:], int(port), torrent)
-        print tracker.peers
+    peers = self._get_peers(torrent)
+    print peers
