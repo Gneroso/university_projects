30eabd012d986502fb9c3987375a6c5cbac59745
vtemian <vladtemian@gmail.com>
2014-05-05 12:14:49 +0300

    Added all necesery args form spec

--- data_structures/bitorrent/server/announce/api.py
+++ data_structures/bitorrent/server/announce/api.py
@@ -1,9 +1,14 @@
 from flask import Blueprint
 
+from utils.decorators import required
+
 
 endpoint = Blueprint('announce', __name__)
 
 
-@endpoint.route('/announce')
-def announce():
+@endpoint.route('/announce', methods=['GET'])
+@required('info_hash', 'peer_id', 'port', 'uploaded', 'downloaded', 'left',
+          'compact', 'no_peer_id', 'event')
+def announce(info_hash, peer_id, port, uploaded, downloaded, left, compact,
+             no_peer_id, event):
   return 'working'
