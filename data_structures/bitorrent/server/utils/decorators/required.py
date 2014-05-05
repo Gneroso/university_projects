fa99b45af0af6a2d30efac71d692a19000de7580
vtemian <vladtemian@gmail.com>
2014-05-05 11:11:28 +0300

    Usefull for tcp handler

---
+++ data_structures/bitorrent/server/utils/decorators/required.py
@@ -0,0 +1,35 @@
+from functools import wraps
+
+from flask import request
+
+from utils.exceptions import HttpBadRequest
+
+
+class required(object):
+
+  def __init__(self, *requires):
+    self.requires = requires
+
+  def get_arguments(self):
+    if request.method in ['POST', 'PUT']:
+      return request.data
+    return request.args
+
+  def __call__(self, f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+      errors = []
+      arguments = self.get_arguments()
+
+      for param in self.requires:
+        if param not in arguments:
+          errors.append('%s is required' % param)
+        else:
+          kwargs[param] = self.requires[param]
+
+      if errors:
+        raise HttpBadRequest("\n".join(errors))
+
+      result = f(*args, **kwargs)
+      return result
+    return decorated
