fcb6241d2c62899435c8b879748206b8db953634
vtemian <vladtemian@gmail.com>
2014-05-16 21:56:23 +0300

    Made our life easier using templates

---
+++ c++/routing_simulation/vector.h
@@ -0,0 +1,9 @@
+template <class T>
+class vector {
+    T *v;
+    int size;
+  public:
+    void add(T x);
+};
+
+#include "vector.tpp"
---
+++ c++/routing_simulation/vector.tpp
@@ -0,0 +1,4 @@
+template<class T> void vector<T>::add(T x) {
+  v[size] = x;
+  size += 1;
+}
