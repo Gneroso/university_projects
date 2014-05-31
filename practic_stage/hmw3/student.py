892f44756c160388c5ac00fb0d1b9ab37cb09bb0
vtemian <vladtemian@gmail.com>
2014-05-31 16:43:48 +0300

    Impement sorting strategy

--- practic_stage/hmw3/student.py
+++ practic_stage/hmw3/student.py
@@ -5,7 +5,28 @@ class Student(object):
     self.id = id
     self.grades = grades
 
+  def __repr__(self):
+    return "< %s >" % self.name
+
+  @property
+  def total(self):
+    return sum([self.grades[field] for field in self.grades])
+
+  def __lt__(self, another_student):
+    return self.total < another_student.total
+
+  def __gt__(self, another_student):
+    return self.total > another_student.total
+
 
 class Students(list):
+  def __init__(self, strategy):
+    self.strategy = strategy
+
   def sort_by(self, grade):
-    pass
+    strategy = self.strategy(self)
+    return strategy.sort_by(grade)
+
+  def sort(self):
+    strategy = self.strategy(self)
+    return strategy.sort()
